f4150420207c2003e3642b88c5b07c0a30c8bd09
Lucas Messias <luquinhasmessias7@gmail.com>
2024-10-11 11:18:30 -0300

    adição coluna valor calculado

--- faturator.py
+++ faturator.py
@@ -15,25 +15,28 @@ if inserir_arquivo is not None:
 
 
 valores = {
-    'Static Maps': [13.20, 10.56, 7.92],
-    'Dynamic Maps': [46.20, 36.96, 27.72],
-    'Directions' : [33.00, 26.40, 19.80],
-    'Directions Advanced' : [66.00, 52.80, 39.60],
-    'Geocoding': [33.00, 26.40, 19.80]
+    'Itens' : ['Static Maps','Dynamic Maps', 'Directions','Directions Advanced', 'Geocoding'],
+    'valor1' : [13.20, 46.20, 33.00, 66.00, 33.00],
+    'valor2' : [10.56, 36.96, 26.40, 52.80, 26.40 ],
+    'valor3' : [7.92, 27.72, 19.80, 39.60, 19.80],
 }
 
+valores = pd.DataFrame(valores)
 
-parametros = [
-    100000, 500000, 1000000
-]
+quantidade_uso1 = 100000
+quantidade_uso2= 500000
+quantidade_uso3 = 1000000
 
 
 
-# static maps = preco1 : 13.20, preco2 : 10.56, preco3: 7.92
-# dynamic maps = preco1 : 46.20, preco2 : 36.96, preco3: 27.72
-# directions = preco1: 33.00, preco2 : 26.40, preco3: 19.80
-# Direction advanced = preco1: 66.00, preco2: 52.80, preco3: 39.60
-# geocoding = preco1: 33.00, preco2: 26.40, preco3: 19.80
+
+def calcula_valor(quantidade,valor1,valor2,valor3,quantidade_uso1,quantidade_uso2,quantidade_uso3):
+    if quantidade <= quantidade_uso1:
+        return quantidade * valor1
+    elif quantidade <= quantidade_uso2:
+        return (quantidade_uso1 * valor1) + (quantidade - quantidade_uso1) * valor2
+    else:
+        return quantidade_uso1 * valor1 + quantidade_uso2 * valor2 + (quantidade - quantidade_uso1 - quantidade_uso2) * valor3
 
 
 
@@ -44,7 +47,6 @@ colunas_requisitadas = colunas_requisitadas.drop(index=11)
 
 
 st.text('TABELA INICIAL')
-st.write(colunas_requisitadas)
 
 if inserir_arquivo is not None:
 
@@ -71,13 +73,24 @@ if inserir_arquivo is not None:
     )
     colunas_requisitadas['Desconto']=colunas_requisitadas['Desconto'].astype(str).str.replace(',','', regex= False).astype(float)
 
+    colunas_requisitadas=colunas_requisitadas.drop(columns='Custo não arredondado (R$)', index=6)
 
-
-    colunas_requisitadas=colunas_requisitadas.drop(columns='Custo não arredondado (R$)')
-    st.text('TABELA ATUALIZADA')
     st.write(colunas_requisitadas)
 
+    colunas_requisitadas['valor calculado'] = colunas_requisitadas.apply(lambda row: calcula_valor(
+    row['Quantidade de uso'],
+    valores.loc[valores['Itens'] == row['Descrição da SKU'], 'valor1'].values,
+    valores.loc[valores['Itens'] == row['Descrição da SKU'], 'valor2'].values,
+    valores.loc[valores['Itens'] == row['Descrição da SKU'], 'valor3'].values,
+    quantidade_uso1,quantidade_uso2,quantidade_uso3), axis=1)
+    
+    colunas_requisitadas['valor calculado'] = colunas_requisitadas['valor calculado']
+    
+    
 
+    st.text('TABELA ATUALIZADA')
+    st.write(colunas_requisitadas)
+    
 
 else:
     'Adicione um arquivo CSV'
